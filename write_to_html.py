de00e51ceeb92dc42c2305814b230f01b40d9491
hayrdenhald <hayrdenhald@fastmail.com>
2023-06-18 18:20:53 +0200

    Add try-except

--- write_to_html.py
+++ write_to_html.py
@@ -41,28 +41,28 @@ class HtmlWriter():
     
     @staticmethod
     def write_scores_to_html(index_to_score: dict) -> tuple[bool, str]:
-        # try:
-        with open(FILE_PATH, 'r') as html:
-            soup = BeautifulSoup(html, 'lxml')
-            scores_ul = soup.find("ul", {"id": "scores"})
-            assert isinstance(scores_ul, Tag)
+        try:
+            with open(FILE_PATH, 'r') as html:
+                soup = BeautifulSoup(html, 'lxml')
+                scores_ul = soup.find("ul", {"id": "scores"})
+                assert isinstance(scores_ul, Tag)
 
-            new_scores_ul = soup.new_tag("ul", attrs={"id": "scores"})
+                new_scores_ul = soup.new_tag("ul", attrs={"id": "scores"})
 
-            for idx, score in index_to_score.items():
-                new_score_li = soup.new_tag("li", attrs={"id": f"score-{idx}"})
-                new_score_li.insert(0, f"{idx}: {score}")
-                new_scores_ul.append(new_score_li)
-            
-            scores_ul.replace_with(new_scores_ul)
+                for idx, score in index_to_score.items():
+                    new_score_li = soup.new_tag("li", attrs={"id": f"score-{idx}"})
+                    new_score_li.insert(0, f"{idx}: {score}")
+                    new_scores_ul.append(new_score_li)
+                
+                scores_ul.replace_with(new_scores_ul)
 
-        with open(FILE_PATH, "wb") as file:
-            file.write(soup.prettify("utf-8"))
+            with open(FILE_PATH, "wb") as file:
+                file.write(soup.prettify("utf-8"))
 
-        success = True
-        error = ""
-        return (success, error)
-        # except Exception as ex:
-        #     success = False
-        #     error = str(ex)
-        #     return (success, error)
+            success = True
+            error = ""
+            return (success, error)
+        except Exception as ex:
+            success = False
+            error = str(ex)
+            return (success, error)
